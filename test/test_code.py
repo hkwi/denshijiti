04e67b93a4d8200326deb51da802775f794a36c3
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-10-28 20:37:12 +0900

    add copy operation

--- test/test_code.py
+++ test/test_code.py
@@ -1,6 +1,10 @@
+import os.environ
 import unittest
 import denshijiti
+import shutil
+import os.path
 
 class TestCode(unittest.TestCase):
 	def test_code(self):
 		denshijiti.run_code()
+		shutil.copy("code.ttl", os.path.join(os.path.dirname(__file__), "../docs/"))
