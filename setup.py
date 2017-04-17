7d6bfc8f984ab765863fde271538ed6e6cb25589
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2017-04-17 21:13:53 +0900

    maintainance

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name='denshijiti',
 	version='0.1.0',
 	description='Japan denshijiti/code RDF generator',
-	long_description=open("README.rst").read(),
+	long_description=open("README.md").read(),
 	author='Hiroaki Kawai',
 	author_email='hiroaki.kawai@gmail.com',
 	url='https://github.com/hkwi/denshijiti/',
